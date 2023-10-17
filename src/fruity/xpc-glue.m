e465535374ed2fce63489a97ac10838c7faa1d03
Ole André Vadla Ravnås <oleavr@gmail.com>
2023-10-17 15:59:55 +0200

    [WIP] Experiment with Network

--- src/fruity/xpc-glue.m
+++ src/fruity/xpc-glue.m
@@ -1,85 +1,48 @@
 #include "frida-core.h"
 
 #import <Foundation/Foundation.h>
-#include <dlfcn.h>
 #include <dispatch/dispatch.h>
-#include <xpc/xpc.h>
-
-typedef xpc_object_t remote_device_t;
-
-static void frida_dump_device (remote_device_t device, bool connected);
-
-static void (* remote_device_browse_present) (int kind, dispatch_queue_t queue, void (^ callback) (remote_device_t device, bool is_last_item));
-static xpc_object_t (* remote_device_copy_properties) (remote_device_t self);
-static xpc_object_t (* remote_device_copy_service_names) (remote_device_t self);
-static xpc_object_t (* remote_device_copy_local_service_names) (remote_device_t self);
-static bool (* remote_device_get_connectable) (remote_device_t self);
-static void (* remote_device_set_connected_callback) (remote_device_t self, dispatch_queue_t queue, void (^ callback) (remote_device_t device));
-static void (* remote_device_heartbeat) (remote_device_t self, dispatch_queue_t queue, void (^ callback) (bool alive));
+#include <Network/Network.h>
 
 void
 _frida_xpc_get_endpoint_for_device (const gchar * uuid)
 {
-  void * rsd;
   dispatch_queue_t queue;
-
-  rsd = dlopen ("/System/Library/PrivateFrameworks/RemoteServiceDiscovery.framework/RemoteServiceDiscovery", RTLD_GLOBAL | RTLD_LAZY);
-  remote_device_browse_present = dlsym (rsd, "remote_device_browse_present");
-  remote_device_copy_properties = dlsym (rsd, "remote_device_copy_properties");
-  remote_device_copy_service_names = dlsym (rsd, "remote_device_copy_service_names");
-  remote_device_copy_local_service_names = dlsym (rsd, "remote_device_copy_local_service_names");
-  remote_device_get_connectable = dlsym (rsd, "remote_device_get_connectable");
-  remote_device_set_connected_callback = dlsym (rsd, "remote_device_set_connected_callback");
-  remote_device_heartbeat = dlsym (rsd, "remote_device_heartbeat");
+  nw_browse_descriptor_t desc;
+  nw_browser_t browser;
 
   queue = dispatch_queue_create ("re.frida.fruity.queue", DISPATCH_QUEUE_SERIAL);
 
-  remote_device_browse_present (8, queue, ^(remote_device_t device, bool is_last_item)
+  desc = nw_browse_descriptor_create_bonjour_service ("_remotepairing._tcp", "local.");
+  nw_parameters_t params = nw_parameters_create_secure_tcp (NW_PARAMETERS_DISABLE_PROTOCOL, NW_PARAMETERS_DEFAULT_CONFIGURATION);
+
+  browser = nw_browser_create (desc, params);
+  nw_browser_set_queue (browser, queue);
+  nw_browser_set_browse_results_changed_handler (browser, ^(nw_browse_result_t old_result, nw_browse_result_t new_result, bool batch_complete)
       {
-        g_print ("got callback!\n");
-        NSLog (@"\tdevice: %@", device);
-        g_print ("\tis_last_item: %s\n", is_last_item ? "true" : "false");
-        if (device == nil)
-          return;
+        NSLog (@"results_changed()\n\told_result=%@\n\tnew_result=%@\n\tbatch_complete=%s",
+            old_result,
+            new_result,
+            batch_complete ? "true" : "false");
 
-        frida_dump_device (device, false);
+        nw_endpoint_t ep = nw_browse_result_copy_endpoint (new_result);
+        NSLog (@"endpoint: %@", ep);
 
-        remote_device_set_connected_callback (device, queue, ^(remote_device_t device)
+        nw_txt_record_t txt = nw_browse_result_copy_txt_record_object (new_result);
+        NSLog (@"txt: %@", txt);
+
+        NSLog (@">>>");
+        nw_browse_result_enumerate_interfaces (new_result, ^bool (nw_interface_t iface)
             {
-              g_print ("connected! device=%p\n", device);
-              frida_dump_device (device, false);
-
-              xpc_retain (device);
-
-              remote_device_heartbeat (device, queue, ^(bool alive)
-                  {
-                    g_print ("heartbeat! alive=%s\n", alive ? "true" : "false");
-                    frida_dump_device (device, true);
-                    xpc_release (device);
-                  });
+              NSLog (@"interface: %@", iface);
+              return true;
             });
+        NSLog (@"<<<\n");
       });
+  nw_browser_start (browser);
+
+  nw_release (params);
+  nw_release (desc);
 
   g_usleep (5 * G_USEC_PER_SEC);
-}
-
-static void
-frida_dump_device (remote_device_t device, bool connected)
-{
-  xpc_object_t props = remote_device_copy_properties (device);
-  NSLog (@"\tproperties: %@", props);
-
-  xpc_object_t names = remote_device_copy_service_names (device);
-  NSLog (@"\tservices names: %@", names);
-
-#if 0
-  xpc_object_t local_names = remote_device_copy_local_service_names (device);
-  NSLog (@"\tlocal services names: %@", local_names);
-#endif
-
-  if (connected)
-  {
-    bool connectable = remote_device_get_connectable (device);
-    NSLog (@"\tconnectable: %s", connectable ? "true" : "false");
-  }
 }
